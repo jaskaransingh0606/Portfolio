8c54b1ef1feb71bf600cc13fe7222d752a84beb0
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-07-17 20:08:35 +0530

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Hero, Navbar,  Feedback,Experience ,Tech,Works} from './components'
+import React from 'react'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+
+      <div className='relative z-0 primary w-full'>
+      <Navbar />
+        <div className='bg-hero-pattern bg-cover w-full w-vh bg-no-repeat '>
+          
+          <Hero />
+        </div>
+
+        <div>
+          {/* <About />
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedback/> */}
+
+          <div>
+            {/* <Contact /> */}
+            
+          </div>
+
+        </div>
+
+
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
